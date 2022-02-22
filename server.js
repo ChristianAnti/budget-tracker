966eb103020542349b96d99a511db8d71d780e03
ChristianAnti <89366348+ChristianAnti@users.noreply.github.com>
2022-02-21 22:53:17 -0500

    deploying

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3000;
-const PORT = 3000;
-
 const app = express();
 
 app.use(logger("dev"));
@@ -16,12 +14,16 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/budgetDB',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
-// routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
